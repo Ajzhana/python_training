6e380b9c827a69737f665307e39e2afacda3edc2
Aizhana <a.nurgalieva@robotvera.com>
2018-07-29 18:29:25 +0400

    Тест размечен комментариями

--- test_add_contact.py
+++ test_add_contact.py
@@ -14,10 +14,12 @@ class test_add_contact (unittest.TestCase):
         self.wd = WebDriver(capabilities={"marionette": False})
         self.wd.implicitly_wait(60)
     
-    def test_(self):
+    def test_add_contact(self):
         success = True
         wd = self.wd
+        # open home page
         wd.get("http://localhost/addressbook/")
+        # login
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
@@ -26,8 +28,9 @@ class test_add_contact (unittest.TestCase):
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+        # init contact creation
         wd.find_element_by_link_text("add new").click()
-        wd.find_element_by_name("theform").click()
+        # fill group form
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys("1233")
@@ -62,7 +65,9 @@ class test_add_contact (unittest.TestCase):
         wd.find_element_by_name("address2").click()
         wd.find_element_by_name("address2").clear()
         wd.find_element_by_name("address2").send_keys("dddssss")
+        # submit form creation
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+        # logout
         wd.find_element_by_link_text("Logout").click()
         self.assertTrue(success)
     
