2e201e5b8a3a8bf703a5195a03b89b1f620cce5a
Aizhana <a.nurgalieva@robotvera.com>
2018-07-31 15:19:18 +0400

    Выделены классы для работы с сессиями и контактами

---
+++ fixture/group2.py
@@ -0,0 +1,45 @@
+
+
+class GroupHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def submit_form_creation(self):
+        wd = self.app.wd
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+
+    def fill_form(self, contact):
+        wd = self.app.wd
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(contact.company)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.address)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.home)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.mobile)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        wd.find_element_by_name("address2").click()
+        wd.find_element_by_name("address2").clear()
+        wd.find_element_by_name("address2").send_keys(contact.address2)
+        self.submit_form_creation()
+
+    def init_contact_creation(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("add new").click()
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -10,9 +10,9 @@ def app(request):
     return fixture
     
 def test_add_contact(app):
-    app.login("admin", "secret")
-    app.init_contact_creation()
-    app.fill_form(contact(firstname="1233", middlename="1232323", lastname="dftt", company="wddfdf", home="89655885558", address="dfrfrfff", mobile="855885485", email="dddddd",
+    app.session.login("admin", "secret")
+    app.group.init_contact_creation()
+    app.group.fill_form(contact(firstname="1233", middlename="1232323", lastname="dftt", company="wddfdf", home="89655885558", address="dfrfrfff", mobile="855885485", email="dddddd",
                           address2="weeerdddd"))
-    app.logout()
+    app.session.logout()
 
