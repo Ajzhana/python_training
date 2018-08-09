d28fe11191c57fad77564900d1fe31bc78d4e7a1
Aizhana <a.nurgalieva@robotvera.com>
2018-08-09 14:43:58 +0400

    Изменен текст на изменение группы с учетом конструкции if-else

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -2,9 +2,12 @@
 
 from model.group import Group
 
-def test_edit_first_group(app):
+def test_modify_group_name(app):
     app.session.login(username="admin", password="secret")
-    app.group.init_edit_first_group()
-    app.group.fill_form(Group(name="ryryyr", header="rrr", footer="rrrr"))
-    app.group.submit_update()
+    app.group.modify_first_group(Group(name="New group"))
+    app.session.logout()
+
+def test_modify_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(header="New header"))
     app.session.logout()
