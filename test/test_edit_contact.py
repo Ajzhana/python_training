89309a7ea0a9b55cdbf6c181cc30a65442a4e5f0
Aizhana <a.nurgalieva@robotvera.com>
2018-08-16 12:00:14 +0400

    Доработан тесты на изменение группы и контакта. Теперь сравниваются отсортированные списки

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -7,16 +7,20 @@ def test_edit_contact_firstname(app):
         app.contact.fill_form(contact(firstname="Test"))
         app.contact.submit_form_creation()
     old_contacts = app.contact.get_contact_list()
-    app.contact.edit_contact(contact(firstname="New firstname"))
+    cont = contact(firstname="New firstname", lastname="New lastname")
+    cont.id = old_contacts[0].id
+    app.contact.edit_contact(cont)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = cont
+    assert sorted(old_contacts, key=contact.id_or_max) == sorted(new_contacts, key=contact.id_or_max)
 
-def test_edit_contact_lastname(app):
-    if app.contact.count() == 0:
-        app.contact.init_contact_creation()
-        app.contact.fill_form(contact(lastname="Test"))
-        app.contact.submit_form_creation()
-    old_contacts = app.contact.get_contact_list()
-    app.contact.edit_contact(contact(lastname="New lastname"))
-    new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) == len(new_contacts)
+#def test_edit_contact_lastname(app):
+#    if app.contact.count() == 0:
+#        app.contact.init_contact_creation()
+#        app.contact.fill_form(contact(lastname="Test"))
+#        app.contact.submit_form_creation()
+#    old_contacts = app.contact.get_contact_list()
+#    app.contact.edit_contact(contact(lastname="New lastname"))
+#    new_contacts = app.contact.get_contact_list()
+#    assert len(old_contacts) == len(new_contacts)
