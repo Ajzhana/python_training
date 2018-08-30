13d7a81c332de232a445c991298ebc16af82296e
Aizhana <a.nurgalieva@robotvera.com>
2018-08-30 16:57:26 +0400

    Для теста test_add_group тестовые данные вынесены в отдельный файл

--- conftest.py
+++ conftest.py
@@ -11,7 +11,7 @@ def app(request):
     global fixture
     global target
     browser = request.config.getoption("--browser")
-    target = request.config.getoption("--target")
+    #target = request.config.getoption("--target")
     if target is None:
         config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
         with open(config_file) as f:
