1abd4afe4243110f5f553693c3be433d3c0a1cd3
Aizhana <a.nurgalieva@robotvera.com>
2018-08-31 15:09:09 +0400

    генератор тестовых данных + загрузка из файла .json (контакты)

---
+++ generator/contact.py
@@ -0,0 +1,47 @@
+from model.contact import contact
+import random
+import string
+import os.path
+import jsonpickle
+import getopt
+import sys
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of contacts", "file"])
+except getopt.GetoptError as err:
+    getopt.usege()
+    sys.exit(2)
+
+n = 2
+f = "data/contacts.json"
+
+for o, a in opts:
+    if o == "-n":
+        n = int(a)
+    elif o == "-f":
+        f = a
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [
+    contact(firstname=random_string("firstname", 10),
+            middlename=random_string("middlename", 10),
+            lastname=random_string("lastname", 10),
+            company=random_string("company", 10),
+            home=random_string("home", 10),
+            address=random_string("address", 10),
+            mobile=random_string("mobile", 10),
+            email=random_string("email", 10),
+            address2=random_string("address2", 10))
+
+    for i in range(2)
+   ]
+
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
+
+with open(file, "w") as out:
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(testdata))
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,30 +1,9 @@
 # -*- coding: utf-8 -*-
 from model.contact import contact
-from sys import maxsize
-import pytest
-import random
-import string
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-testdata = [
-    contact(firstname=random_string("firstname", 10),
-            middlename=random_string("middlename", 10),
-            lastname=random_string("lastname", 10),
-            company=random_string("company", 10),
-            home=random_string("home", 10),
-            address=random_string("address", 10),
-            mobile=random_string("mobile", 10),
-            email=random_string("email", 10),
-            address2=random_string("address2", 10))
-
-    for i in range(2)
-   ]
-
-@pytest.mark.parametrize("cont", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, cont):
+def test_add_contact(app, json_contacts):
+    cont = json_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.init_contact_creation()
     app.contact.fill_form(cont)
