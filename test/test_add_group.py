4350c3880de671b67a871aa1d093667f77438ebe
Aizhana <a.nurgalieva@robotvera.com>
2018-09-03 18:53:42 +0400

    Переделываем тесты: проверяем данные, загруженные из базы данных для групп

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,14 +2,13 @@
 from model.group import Group
 
 
-def test_add_group(app, json_groups):
+def test_add_group(app, db, json_groups):
     group = json_groups
-    old_groups = app.group.get_group_list()
+    old_groups = db.get_group_list()
     app.group.init_creation()
     app.group.fill_form(group)
     app.group.submit_creation()
-    assert len(old_groups) + 1 == app.group.count()
-    new_groups = app.group.get_group_list()
+    new_groups = db.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
